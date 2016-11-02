709af025cefe979f24413a101aa51cd6757514fc
AnikSarker <aniksarkerbuet1997@gmail.com>
2016-11-02 16:35:39 +0600

    Add files via upload

---
+++ UVA 11597 - Spanning Subtree.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    int cse=0;
+    while(1){
+        scanf("%d",&n);
+        if(n==0) break;
+        printf("Case %d: %d\n",++cse,n/2);
+    }
+}
